53851d346602aac303b1f6bed3ba93f21d9c4598
Luis Felipe Garcia <fellgar246@gmail.com>
2024-09-02 22:03:47 -0600

    refactor todo item component to use reactive forms and add toggle functionality

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+import { Store } from '@ngrx/store';
+import * as actions from '../todo.actions';
+
+import { Todo } from '../models/todo.model';
+import { AppState } from '../../app.reducer';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.css'
 })
-export class TodoItemComponent {
+export class TodoItemComponent implements OnInit {
 
+  @Input() todo!: Todo;
+  @ViewChild('inputFisico') txtInputFisico!: ElementRef;
+
+  chkCompletado!: FormControl;
+  txtInput!: FormControl;
+
+  editando: boolean = false;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+
+    this.chkCompletado = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+
+    this.chkCompletado.valueChanges.subscribe(valor => {
+      this.store.dispatch(actions.toggle({ id: this.todo.id }));
+
+    });
+  }
+
+  editar() {
+    this.editando = true;
+    this.txtInputFisico.nativeElement.select();
+  }
+
+  terminarEdicion() {
+    this.editando = false;
+  }
 }
--- src/app/todos/todo.actions.ts
+++ src/app/todos/todo.actions.ts
@@ -4,3 +4,8 @@ export const crear = createAction(
   '[Todo] Crear Todo',
   props<{ texto: string }>()
 );
+
+export const toggle = createAction(
+  '[Todo] Toggle Todo',
+  props<{ id: number }>()
+);
