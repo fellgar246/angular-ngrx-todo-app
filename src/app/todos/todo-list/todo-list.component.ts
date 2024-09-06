c8c79e5533b28580313189e9a9141868054fe943
Luis Felipe Garcia <fellgar246@gmail.com>
2024-09-06 16:54:53 -0600

    add footer options and filter properties

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 
 import { Todo } from '../models/todo.model';
 import { AppState } from '../../app.reducer';
+import { filtrosValidos } from '../../filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-list',
@@ -12,14 +13,18 @@ import { AppState } from '../../app.reducer';
 export class TodoListComponent implements OnInit {
 
   todos: Todo[] = [];
+  filtroActual: filtrosValidos = 'todos';
 
   constructor( private store: Store<AppState> ) { }
 
   ngOnInit(): void {
-    this.store.select('todos').subscribe( todos => {
+    // this.store.select('todos').subscribe( todos => {
+    //   this.todos = todos;
+    //   console.log('Todos:', todos);
+    // });
+    this.store.subscribe( ({todos , filtro}) => {
       this.todos = todos;
-      console.log('Todos:', todos);
+      this.filtroActual = filtro;
     });
   }
-
 }
